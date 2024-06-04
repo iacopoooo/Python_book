7b83252004b48ceab3c9e0416711d420da12ba40
iacopoooo <ico_vh@yahoo.it>
2024-06-04 19:37:32 +0200

    Add files via upload

---
+++ pythonbook2_Age.py
@@ -0,0 +1,17 @@
+
+print('Verifica maggiorenne\n')
+
+# L'input dell'età viene convertito direttamente in intero
+
+eta = int(input("Inserisci la tua età: "))
+
+# Controllo dell'età con una struttura if-else correttamente indentata
+
+if eta >= 18:
+
+    print("Sei maggiorenne.")
+
+else:
+
+    print("Sei minorenne.")
+
