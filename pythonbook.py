e647caa6aafb633b0d076c9ca657fb4e363da948
iacopoooo <ico_vh@yahoo.it>
2024-06-04 11:13:55 +0200

    Add files via upload

---
+++ pythonbook.py
@@ -0,0 +1,13 @@
+#esempio 1
+print ("ciao come stai?")
+print ("Molto bene. Grazie!")
+
+#esempio2
+print("Questa frase è su\ndue righe in Python.")# \n va a capo
+
+#esempio3
+frase1= "CIAO COME STAI?"
+frase2= "MOLTO BENE. GRAZIE!"
+print(frase1)
+print(frase2)
+
